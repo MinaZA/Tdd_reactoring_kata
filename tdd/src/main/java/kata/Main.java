380c1f4c54ccaf52837691fb4d4e8e7d4e5b3f0e
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2024-06-27 16:29:29 +0200

    refacto_valide_du code

--- tdd/src/main/java/kata/Main.java
+++ tdd/src/main/java/kata/Main.java
@@ -1,43 +1,17 @@
 package kata;
 
 public class Main {
+    private static final int[] VALUES = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] SYMBOLS = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
     public static String convert(int number) {
         StringBuilder result = new StringBuilder();
-        while (number >= 100) {
-            result.append("C");
-            number -= 100;
-        }
-        if (number >= 90) {
-            result.append("XC");
-            number -= 90;
-        }
-        if (number >= 50) {
-            result.append("L");
-            number -= 50;
-        }
-        if (number >= 40) {
-            result.append("XL");
-            number -= 40;
-        }
-        while (number >= 10) {
-            result.append("X");
-            number -= 10;
-        }
-        if (number >= 9) {
-            result.append("IX");
-            number -= 9;
-        }
-        if (number >= 5) {
-            result.append("V");
-            number -= 5;
-        }
-        if (number >= 4) {
-            result.append("IV");
-            number -= 4;
-        }
-        while (number >= 1) {
-            result.append("I");
-            number -= 1;
+
+        for (int i = 0; i < VALUES.length; i++) {
+            while (number >= VALUES[i]) {
+                result.append(SYMBOLS[i]);
+                number -= VALUES[i];
+            }
         }
 
         return result.toString();
