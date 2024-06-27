62b9c48b02fbd0a8367dfe76282846176d0d3f6b
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2024-06-27 15:59:37 +0200

    refacto_test_validé

--- tdd/src/main/java/kata/Main.java
+++ tdd/src/main/java/kata/Main.java
@@ -11,13 +11,13 @@ public class Main {
             result.append("IX");
             number -= 9;
         }
-        if (number >= 4) {
-            result.append("IV");
-            number -= 4;
-        }
         if (number >= 5) {
             result.append("V");
             number -= 5;
+        }
+        if (number >= 4) {
+            result.append("IV");
+            number -= 4;
         }
         while (number >= 1) {
             result.append("I");
